1a33697519eceaa9103e242d9623e43f1aede107
BHAWANI SINGH SHEKHAWAT <bhawani.hmh@gmail.com>
2017-02-17 17:57:22 +0530

    Update Channel.java

--- src/main/java/iptv/npvr/pojo/Channel.java
+++ src/main/java/iptv/npvr/pojo/Channel.java
@@ -2,28 +2,17 @@ package iptv.npvr.pojo;
 
 import java.io.Serializable;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-
 /**
  * 
  * @author bhawani.singh
  *
  */
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({ "channelId", "callSign", "isFallBack" })
 public class Channel implements Serializable {
 
-	@JsonProperty("channelId")
 	private String channelId;
-	@JsonProperty("callSign")
 	private String callSign;
-	@JsonProperty("isFallBack")
 	private boolean isFallBack;
 	
-	@JsonIgnore
 	private final static long serialVersionUID = 1117074953613238204L;
 
 	/**
@@ -44,32 +33,26 @@ public class Channel implements Serializable {
 		this.callSign = callSign;
 	}
 
-	@JsonProperty("channelId")
 	public String getChannelId() {
 		return channelId;
 	}
 
-	@JsonProperty("channelId")
 	public void setChannelId(String channelId) {
 		this.channelId = channelId;
 	}
 
-	@JsonProperty("callSign")
 	public String getCallSign() {
 		return callSign;
 	}
 
-	@JsonProperty("callSign")
 	public void setCallSign(String callSign) {
 		this.callSign = callSign;
 	}
 
-	@JsonProperty("isFallBack")
 	public boolean isFallBack() {
 		return isFallBack;
 	}
 
-	@JsonProperty("isFallBack")
 	public void setFallBack(boolean isFallBack) {
 		this.isFallBack = isFallBack;
 	}
