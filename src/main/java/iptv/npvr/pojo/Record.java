7b568d0c5f7ee35dad5f2e6ea792ff71e860678d
BHAWANI SINGH SHEKHAWAT <bhawani.hmh@gmail.com>
2017-02-17 18:01:50 +0530

    Update Record.java

--- src/main/java/iptv/npvr/pojo/Record.java
+++ src/main/java/iptv/npvr/pojo/Record.java
@@ -3,29 +3,18 @@ package iptv.npvr.pojo;
 
 import java.io.Serializable;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-
 /**
  * 
  * @author bhawani.singh
  *
  */
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({ "channelId", "programId", "startTime", "endTime" })
 public class Record implements Serializable {
 
-	@JsonProperty("channelId")
 	private String channelId;
-	@JsonProperty("programId")
 	private String programId;
-	@JsonProperty("startTime")
 	private String startTime;
-	@JsonProperty("endTime")
 	private String endTime;
-	@JsonIgnore
+
 	private final static long serialVersionUID = 2063548155558844998L;
 
 	/**
@@ -50,42 +39,34 @@ public class Record implements Serializable {
 		this.endTime = endTime;
 	}
 
-	@JsonProperty("channelId")
 	public String getChannelId() {
 		return channelId;
 	}
 
-	@JsonProperty("channelId")
 	public void setChannelId(String channelId) {
 		this.channelId = channelId;
 	}
 
-	@JsonProperty("programId")
 	public String getProgramId() {
 		return programId;
 	}
 
-	@JsonProperty("programId")
 	public void setProgramId(String programId) {
 		this.programId = programId;
 	}
 
-	@JsonProperty("startTime")
 	public String getStartTime() {
 		return startTime;
 	}
 
-	@JsonProperty("startTime")
 	public void setStartTime(String startTime) {
 		this.startTime = startTime;
 	}
 
-	@JsonProperty("endTime")
 	public String getEndTime() {
 		return endTime;
 	}
 
-	@JsonProperty("endTime")
 	public void setEndTime(String endTime) {
 		this.endTime = endTime;
 	}
